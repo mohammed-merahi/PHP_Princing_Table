f4ef8b5fd14b6aff93bc0ad07b0cbc99ba900925
Mohammed Merahi <merahi.05016@gmail.com>
2018-05-11 10:45:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# PHP_Princing_Table
---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$SETTINGS["hostname"]='localhost';
+$SETTINGS["mysql_user"]='REPLACE';
+$SETTINGS["mysql_pass"]='REPLACE';
+$SETTINGS["mysql_database"]='REPLACE';
+$SETTINGS["data_table"]='registrations';
+$SETTINGS["paypal_address"]='email@domain.com';
+?>
